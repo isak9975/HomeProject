64d7540031defb5ef897f986792d18f06be393fa
isak9975 <isak9975@gmail.com>
2025-06-30 21:51:01 +0900

    back

---
+++ Back/homeproject/src/test/java/com/korea/homeproject/HomeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korea.homeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
